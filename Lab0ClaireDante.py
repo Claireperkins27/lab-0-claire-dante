6672e3a65de7a9050281d90646d400ca1f89b8b0
claireperkins <claire@fperkins.com>
2020-09-03 11:59:40 -0400

    I added statements about Claire & Dante.

--- Lab0ClaireDante.py
+++ Lab0ClaireDante.py
@@ -1,4 +1,12 @@
-# Team Names
+# Claire, Dante
 # Course: CS151, Dr. Rajeev
-# Date:
-# Lab Number:
+# Date: 09/03/2002
+# Lab Number: 0
+
+print("I am Claire.")
+print("I am from Fairfield, CT.")
+print("I am in student government.")
+
+print("I am Dante.")
+print("I am Norwell, MA.")
+print("I like to play sports.")
