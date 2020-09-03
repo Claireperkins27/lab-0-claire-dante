04b5531374361d43ce54279411d4d7ec958eb616
claireperkins <claire@fperkins.com>
2020-09-03 12:06:51 -0400

    I added added spacing.

--- Lab0ClaireDante.py
+++ Lab0ClaireDante.py
@@ -3,10 +3,10 @@
 # Date: 09/03/2002
 # Lab Number: 0
 
-print("I am Claire.")
+print("\nI am Claire.")
 print("I am from Fairfield, CT.")
 print("I am in student government.")
 
-print("I am Dante.")
+print("\nI am Dante.")
 print("I am Norwell, MA.")
 print("I like to play sports.")
