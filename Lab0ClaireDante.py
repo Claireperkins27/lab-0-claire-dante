9089645beddf115bdb38fb27d26d2de02b5ac54c
claireperkins <claire@fperkins.com>
2020-09-04 13:21:15 -0400

    I added added another statement.

--- Lab0ClaireDante.py
+++ Lab0ClaireDante.py
@@ -6,7 +6,9 @@
 print("\nI am Claire.")
 print("I am from Fairfield, CT.")
 print("I am in student government.")
+print("I am 18 years old.")
 
 print("\nI am Dante.")
 print("I am Norwell, MA.")
 print("I like to play sports.")
+print("I am 19 years old.")
